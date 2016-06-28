b8fb12be57c41e3ebd640c7ad836e374de538fbf
aproschenko.dev@gmail.com <aproschenko.dev@gmail.com>
2016-06-28 16:57:57 +0300

    fix https

--- LocalWeather/include/highcharts.php
+++ LocalWeather/include/highcharts.php
@@ -1,3 +1,3 @@
-<script src="http://code.highcharts.com/highcharts.js" type="text/javascript"></script>
-<script src="http://code.highcharts.com/modules/no-data-to-display.js" type="text/javascript"></script>
+<script src="https://code.highcharts.com/highcharts.js" type="text/javascript"></script>
+<script src="https://code.highcharts.com/modules/no-data-to-display.js" type="text/javascript"></script>
 <script type="text/javascript" src="scripts/kalmanFilter.js"></script>
