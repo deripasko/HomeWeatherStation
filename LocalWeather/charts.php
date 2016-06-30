8bc38cb2875c1369ef3ca35bda7fba454fe3ced4
aproschenko.dev@gmail.com <aproschenko.dev@gmail.com>
2016-06-30 11:42:45 +0300

    menu include

--- LocalWeather/charts.php
+++ LocalWeather/charts.php
@@ -14,37 +14,7 @@ if (!isset($_SESSION["username"])) {
 </head>
 <body>
 
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Показать меню</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="/">Домашняя метеостанция</a>
-        </div>
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li><a href="/">Главная</a></li>
-                <li><a href="/datas.php">Данные</a></li>
-                <li class="active"><a href="/charts.php">Графики</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="/setup.php">Настройки</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION["username"] ?>&nbsp;<span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="/user.php">Личный кабинет</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="/logout.php">Выход</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+<?php include_once("include/menu.php"); ?>
 
 <div class="pageContainer">
 
--- LocalWeather/index.php
+++ LocalWeather/index.php
@@ -13,37 +13,7 @@ if (!isset($_SESSION["username"])) {
 </head>
 <body>
 
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Показать меню</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="/">Домашняя метеостанция</a>
-        </div>
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="/"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;&nbsp;Главная</a></li>
-                <li><a href="/datas.php"><span class="glyphicon glyphicon-align-justify" aria-hidden="true"></span>&nbsp;&nbsp;Данные</a></li>
-                <li><a href="/charts.php"><span class="glyphicon glyphicon-stats" aria-hidden="true"></span>&nbsp;&nbsp;Графики</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="/setup.php"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span>&nbsp;&nbsp;Настройки</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION["username"] ?>&nbsp;<span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="/user.php">Личный кабинет</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="/logout.php">Выход</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+<?php include_once("include/menu.php"); ?>
 
 <div class="pageContainer" id="pageContainer"></div>
 <p class="jumboMessage" id="jumboMessage">Здесь будут отображаться карточки модулей, подключенных к Домашней метеостанции.</p>
